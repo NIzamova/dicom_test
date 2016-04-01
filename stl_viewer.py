8d57fc724163c53a82b60d35b888d9289ed92bd6
lrig <lrig.n@hotmail.com>
2016-04-01 09:41:39 +0300

    add stl with gap

--- stl_viewer.py
+++ stl_viewer.py
@@ -10,7 +10,7 @@ figure = pyplot.figure()
 axes = mplot3d.Axes3D(figure)
 
 # Load the STL files and add the vectors to the plot
-your_mesh = mesh.Mesh.from_file('bones.stl')
+your_mesh = mesh.Mesh.from_file('skull_gap2.stl')
 axes.add_collection3d(mplot3d.art3d.Poly3DCollection(your_mesh.vectors))
 
 # Auto scale to the mesh size
