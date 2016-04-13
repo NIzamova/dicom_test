b3c5fe4fbb6764b9788518c827bcf58f671501c7
lrig <lrig.n@hotmail.com>
2016-04-13 11:25:10 +0300

    haar

--- find_close_conture.py
+++ find_close_conture.py
@@ -1,15 +1,42 @@
 import numpy as np
 import cv2
-import matplotlib.image as mpimg
-import matplotlib.pyplot as plt
+import time
+from matplotlib import pyplot
 
-im = cv2.imread('./test_pos/pos-80.png')
+im = cv2.imread('./test_neg/neg-100.jpg')
 imgray = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
 ret,thresh = cv2.threshold(imgray,127,255,0)
 im2, contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE ,cv2.CHAIN_APPROX_SIMPLE)
 i=0
+pyplot.imsave('./test_neg/neg-100_contours.jpg', im2, cmap=pyplot.cm.bone)
 
 print hierarchy
-if (hierarchy[0][2] <0):
-    print "open "+ i
+print hierarchy[0][0][1]
+time.sleep(1)
 
+cv2.imshow('d', im2)
+cv2.waitKey(0)
+
+
+
+im = cv2.imread('./test_pos/pos-100.jpg')
+imgray = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
+# ret,thresh = cv2.threshold(imgray,255,255,255)
+ret,thresh = cv2.threshold(imgray,127,255,0)
+im2, contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE ,cv2.CHAIN_APPROX_SIMPLE)
+i=0
+pyplot.imsave('./test_neg/pos-100_contours.jpg', im2, cmap=pyplot.cm.bone)
+
+
+print hierarchy
+print hierarchy[0][0][1]
+time.sleep(1)
+
+
+cv2.imshow('dd', im2)
+cv2.waitKey(0)
+
+# for i in contours:
+#     if (hierarchy[0][0][i] <0):
+#         print "open "+ i
+#
---
+++ haar_cascade.py
@@ -0,0 +1,25 @@
+import os
+
+import numpy as np
+import cv2
+from matplotlib import pyplot
+
+Path = "./test_validation/"
+for dirname, dirnames, filenames in os.walk(Path):
+    for filename in filenames:
+        print filename
+        hall_cascade = cv2.CascadeClassifier('cascade.xml')
+        img = cv2.imread(os.path.join(dirname, filename))
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+        faces = hall_cascade.detectMultiScale(gray, 1.3, 5)
+        for (x,y,w,h) in faces:
+            cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+            roi_gray = gray[y:y+h, x:x+w]
+            roi_color = img[y:y+h, x:x+w]
+
+        cv2.imshow('img', img)
+        cv2.waitKey(0)
+
+        pyplot.imsave('./test_validation_result/%s.jpg' %filename, img, cmap=pyplot.cm.bone)
+        print(img)
