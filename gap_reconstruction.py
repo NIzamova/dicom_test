b1174311a3e0bdb82dd8651f806bf82d6cadbf2d
lrig <lrig.n@hotmail.com>
2016-04-22 16:13:29 +0300

    sliser

--- gap_reconstruction.py
+++ gap_reconstruction.py
@@ -4,91 +4,126 @@ from scipy import ndimage
 import math
 import cv2
 from sympy import Point
-
-Path = "./pos_jpg/pos-100.jpg"
-
-a = cv2.imread(Path, 0)
-nonzero = numpy.transpose(numpy.nonzero(a))
-
-
-centerPoint=ndimage.measurements.center_of_mass(a)
-centerPoint=Point(int(centerPoint[0]), int(centerPoint[1]))
-print(centerPoint)
-
-
-contur_list = list()
-gap_borderline_list = list()
-gap_angle_list = list()
-count_point = 0
-radius_begin = 1
-count_point_previous = 0
-
-# TODO for 45, 135, 225, 315 degrees theirself alfa
-radius = min([centerPoint.x, centerPoint.y, 512-centerPoint.x, 512-centerPoint.y])
-
-
-# find gap's begin and gap's end
-# contour_list - point of skull (x, y, alfa)
-# gap_angle_list - alfa and status(begin/end)
-# count_point and count_point_previous help to find gap's begin and end
-for alfa in xrange(0, 360):
-
-    for r in xrange(0, radius):
-        point_y = int(centerPoint.y+r*math.sin(math.radians(alfa)))
-        point_x = int(centerPoint.x+r*math.cos(math.radians(alfa)))
-        if (a[point_x, point_y]):
-            if alfa==0:
-                count_point_previous = 1
-            if count_point_previous == 0:
-                gap_angle_list.append([alfa, str("gap end")])
-
-            count_point = (count_point+1)
-            contur_list.append([point_x, point_y, alfa])
-            count_point_previous=count_point
-
-    if count_point == 0:
-        if count_point_previous!=0 and alfa!=0:
-            gap_angle_list.append([alfa-1, str("gap begin")])
-            count_point_previous=0
-    count_point = 0
-
-
-
-# set alfa for regression +- 20 degrees
-for alfa, stat in gap_angle_list:
-    print alfa, stat
-    if stat == "gap begin":
-        alfa_beg_for_polyfit=alfa-20
-    else:
-        alfa_end_for_polyfit=alfa+20
-
-    for r in xrange(0, radius):
-        point_y = int(centerPoint.y+r*math.sin(math.radians(alfa)))
-        point_x = int(centerPoint.x+r*math.cos(math.radians(alfa)))
-        if (a[point_x, point_y]):
-            gap_borderline_list.append([point_x, point_y])
-
-
-
-
-for (x,y) in gap_borderline_list:
-   print x, y
-
-
-standart_y = numpy.asarray([x for x, y, alfa in contur_list if alfa<alfa_end_for_polyfit and alfa>alfa_beg_for_polyfit], dtype=numpy.int32)
-standart_x = numpy.asarray([y for x, y, alfa in contur_list if alfa<alfa_end_for_polyfit and alfa>alfa_beg_for_polyfit], dtype=numpy.int32)
-
-print (gap_borderline_list[:])
-
 import numpy.polynomial.polynomial as poly
-x_new = numpy.arange(min(gap_borderline_list[:][0]), max(gap_borderline_list[:][0]), 1)
-coefs = poly.polyfit(standart_x, standart_y, 3)
-ffit = poly.polyval(x_new, coefs)
-print x_new, ffit
-pyplot.plot(x_new, ffit)
-pyplot.plot(standart_x,standart_y, 'g')
+
+# Path = "./pos_jpg/pos-112.jpg"
 
 
-pyplot.show()
+def gap_reconstruction_func(img):
+    # a = cv2.imread(img, 0)
+    a = img
+    nonzero = numpy.transpose(numpy.nonzero(a))
+
+    centerPoint=ndimage.measurements.center_of_mass(a)
+    centerPoint=Point(int(centerPoint[0]), int(centerPoint[1]))
+    print(centerPoint)
+
+    contur_list = list()
+    gap_borderline_list = list()
+    gap_angle_list = list()
+    count_point = 0
+    radius_begin = 1
+    count_point_previous = 0
+
+    # TODO for 45, 135, 225, 315 degrees theirself alfa
+    radius = min([centerPoint.x, centerPoint.y, 512-centerPoint.x, 512-centerPoint.y])
+
+
+    # find gap's begin and gap's end
+    # contour_list - point of skull (x, y, alfa)
+    # gap_angle_list - alfa and status(begin/end)
+    # count_point and count_point_previous help to find gap's begin and end
+    for alfa in xrange(0, 360):
+
+        for r in xrange(0, radius):
+            point_y = int(centerPoint.y+r*math.sin(math.radians(alfa)))
+            point_x = int(centerPoint.x+r*math.cos(math.radians(alfa)))
+            if (a[point_x, point_y]):
+                if alfa==0:
+                    count_point_previous = 1
+                if count_point_previous == 0:
+                    if len(gap_angle_list)==0 or abs(gap_angle_list[-1][0] - (alfa)) > 3:
+                        gap_angle_list.append([alfa, str("gap end")])
+
+                count_point = (count_point+1)
+                contur_list.append([point_x, point_y, alfa])
+                count_point_previous = count_point
+
+
+        if count_point == 0:
+            if count_point_previous!=0 and alfa!=0:
+                if len(gap_angle_list)==0 or abs(gap_angle_list[-1][0] - (alfa-1)) > 3:
+                    gap_angle_list.append([alfa-1, str("gap begin")])
+                count_point_previous=0
+        count_point = 0
+
+
+    if len(gap_angle_list)==0:
+        print str("gap not found")
+        return
+
+
+
+    # set alfa for regression +- 20 degrees and set m
+
+    for alfa, stat in gap_angle_list:
+        print alfa, stat
+
+    if gap_angle_list[0][1] == "gap end":
+        tmp=gap_angle_list[0][0]
+        tmp2=gap_angle_list[1][0]
+
+        gap_angle_list[0][0]= tmp2
+        gap_angle_list[1][0]= 360 + tmp
+
+    alfa_beg_for_polyfit=gap_angle_list[0][0]-20
+    alfa_end_for_polyfit=gap_angle_list[1][0]+20
+
+    for alfa, stat in gap_angle_list:
+        print alfa, stat
+        for r in xrange(0, radius):
+            point_y = int(centerPoint.y+r*math.sin(math.radians(alfa)))
+            point_x = int(centerPoint.x+r*math.cos(math.radians(alfa)))
+            if (a[point_x, point_y]):
+                gap_borderline_list.append([point_x, point_y])
+
+
+
+
+    for (x,y) in gap_borderline_list:
+       print x, y
+
+
+    standart_y = numpy.asarray([x for x, y, alfa in contur_list if (alfa<alfa_end_for_polyfit and alfa>alfa_beg_for_polyfit) or (alfa+360<alfa_end_for_polyfit and alfa+360>alfa_beg_for_polyfit)], dtype=numpy.int32)
+    standart_x = numpy.asarray([y for x, y, alfa in contur_list if (alfa<alfa_end_for_polyfit and alfa>alfa_beg_for_polyfit) or (alfa+360<alfa_end_for_polyfit and alfa+360>alfa_beg_for_polyfit)], dtype=numpy.int32)
+
+
+    if (alfa_beg_for_polyfit>0 and alfa_beg_for_polyfit<=180 and alfa_end_for_polyfit>0 and alfa_end_for_polyfit<=180) or (alfa_beg_for_polyfit>180 and alfa_beg_for_polyfit<=360 and alfa_end_for_polyfit>180 and alfa_end_for_polyfit<=360) or (alfa_beg_for_polyfit>360 and alfa_beg_for_polyfit<=540 and alfa_end_for_polyfit>360 and alfa_end_for_polyfit<=540):
+        aa = numpy.asarray([x for x, y in gap_borderline_list])
+        x_new = numpy.arange(min(aa), max(aa), 1)
+        coefs = poly.polyfit(standart_y, standart_x, 3)
+        ffit = poly.polyval(x_new, coefs)
+        result=[pyplot.plot(ffit, x_new, '-b')]
+
+
+    else:
+        aa = numpy.asarray([y for x, y in gap_borderline_list])
+        x_new = numpy.arange(min(aa), max(aa), 1)
+        coefs = poly.polyfit(standart_x, standart_y, 3)
+        ffit = poly.polyval(x_new, coefs)
+        result=[pyplot.plot(x_new, ffit, '-b')]
+
+    #
+    # xxx = numpy.asarray([x for x, y in nonzero])
+    # yyy = numpy.asarray([y for x, y in nonzero])
+    # pyplot.plot(yyy, xxx, 'r')
+
+    pyplot.plot(standart_x,standart_y, 'g')
+    # pyplot.plot(centerPoint.x, centerPoint.y, 'x')
+
+
+
+    pyplot.show()
+    return result
 
 
---
+++ main.py
@@ -0,0 +1,104 @@
+import cv2
+from matplotlib import pyplot
+from plotly.offline import  init_notebook_mode, iplot
+from plotly.graph_objs import *
+from scipy import ndimage
+
+init_notebook_mode()
+
+__author__ = 'N'
+
+from vtk.util import numpy_support
+
+
+import numpy
+from gap_reconstruction import gap_reconstruction_func
+
+
+def vtkImageToNumPy(image, pixelDims):
+    pointData = image.GetPointData()
+    arrayData = pointData.GetArray(0)
+    ArrayDicom = numpy_support.vtk_to_numpy(arrayData)
+    ArrayDicom = ArrayDicom.reshape(pixelDims, order='F')
+
+    return ArrayDicom
+
+def plotHeatmap(array, name="plot"):
+    data = [
+       graph_objs.Heatmap(
+            z=array,
+            colorscale='Greys'
+        )
+    ]
+    layout = graph_objs.Layout(
+        autosize=False,
+        title=name
+    )
+    fig = dict(data=data, layout=layout)
+
+    return iplot(fig)
+
+import vtk
+from IPython.display import Image
+def vtk_show(renderer, width=400, height=300):
+    """
+    Takes vtkRenderer instance and returns an IPython Image with the rendering.
+    """
+    renderWindow = vtk.vtkRenderWindow()
+    renderWindow.SetOffScreenRendering(1)
+    renderWindow.AddRenderer(renderer)
+    renderWindow.SetSize(width, height)
+    renderWindow.Render()
+
+    windowToImageFilter = vtk.vtkWindowToImageFilter()
+    windowToImageFilter.SetInput(renderWindow)
+    windowToImageFilter.Update()
+
+    writer = vtk.vtkPNGWriter()
+    writer.SetWriteToMemory(1)
+    writer.SetInputConnection(windowToImageFilter.GetOutputPort())
+    writer.Write()
+    data = str(buffer(writer.GetResult()))
+
+    return Image(data)
+
+
+PathDicom = "./COU/"
+reader = vtk.vtkDICOMImageReader()
+reader.SetDirectoryName(PathDicom)
+reader.Update()
+
+_extent = reader.GetDataExtent()
+ConstPixelDims = [_extent[1]-_extent[0]+1, _extent[3]-_extent[2]+1, _extent[5]-_extent[4]+1]
+ConstPixelSpacing = reader.GetPixelSpacing()
+
+threshold = vtk.vtkImageThreshold ()
+threshold.SetInputConnection(reader.GetOutputPort())
+threshold.ThresholdByLower(400)  # remove all soft tissue
+threshold.ReplaceInOn()
+threshold.SetInValue(0)  # set all values below 400 to 0
+threshold.ReplaceOutOn()
+threshold.SetOutValue(1)  # set all values above 400 to 1
+threshold.Update()
+
+ArrayDicom = vtkImageToNumPy(threshold.GetOutput(), ConstPixelDims)
+plotHeatmap(numpy.rot90(ArrayDicom[:, :, 0]), name="CT_Thresholded")
+
+dmc = vtk.vtkDiscreteMarchingCubes()
+dmc.SetInputConnection(threshold.GetOutputPort())
+dmc.GenerateValues(1, 1, 1)
+dmc.Update()
+i=0
+for i in xrange(361):
+
+    pyplot.imsave ('./COU_img/im-%d.jpg' %i, ArrayDicom[:,:,i], cmap=pyplot.cm.bone)
+    a = cv2.imread('./COU_img/im-%d.jpg' %i, 0)
+    pyplot.imsave ('./COU_img/im-%d.jpg' %i, ndimage.binary_erosion(a).astype(a.dtype), cmap=pyplot.cm.bone)
+
+
+# for i in xrange(78,81,1):
+#     pyplot.imsave ('./COU_img/im-%d.jpg' %i, ArrayDicom[:,:,i], cmap=pyplot.cm.bone)
+#     a= cv2.imread('./COU_img/im-%d.jpg' %i, 0)
+#     gap_reconstruction_func(ndimage.binary_erosion(a).astype(a.dtype))
+
+# gap_reconstruction_func("./pos_jpg/pos-102.jpg")
